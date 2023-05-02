c862d441b4376cf698fc3333f6062131154a4b2f
AsfandRanglerz <asfand.ranglerz@gmail.com>
2023-05-02 14:55:13 +0500

    changes

--- resources/views/help_center.blade.php
+++ resources/views/help_center.blade.php
@@ -13,23 +13,38 @@
         <div class="container">
             <h1>Testing</h1>
             <img src="{{ asset('public/uploads/website-images/images/help-heart.png') }}" alt="help-heart">
-            <form action="">
-                <div class="form-group">
-                    <label for="">EMAIL ADDRESS <span class="required">*</span></label>
-                    <input type="text" placeholder="Email Address">
-                </div>
-                <div class="form-group">
-                    <label for="">How often do you use eIndustrify?</label>
-                    <input type="text" placeholder="Email Address">
-                </div>
-                <div class="form-group">
-
-                </div>
-            </form>
+            <div class="grey-border px-5 py-4">
+                <form action="">
+                    <div class="form-group">
+                        <label for="">EMAIL ADDRESS <span class="required">*</span></label>
+                        <input type="text" placeholder="Email Address" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="">How often do you use eIndustrify?</label>
+                        <input type="text" placeholder="Email Address" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="">what is your most loved feature of eindustrify website?</label>
+                        <input type="text" placeholder="Type something.." class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="">would you recommend eindustrify to a friend or a colleague?</label>
+                        <input type="text" placeholder="Type something.." class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <div class="form-check d-flex justify-content-between">
+                            <input class="form-check-input" type="checkbox" id="recPerFeedback">
+                            <label class="form-check-label label-light-weight" for="recPerFeedback">Receive personal follow-up to your feedback.</label>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn-bg w-100">Submit Feedback</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </section>
     <!--============================
         CUSTOM PAGES PAGE END
     ==============================-->
 @endsection
-
