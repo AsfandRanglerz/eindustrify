9d020b975c416cfeee637b4767fb9b75baf09952
AsfandRanglerz <asfand.ranglerz@gmail.com>
2023-06-16 20:34:12 +0500

    changes

--- resources/views/product_detail.blade.php
+++ resources/views/product_detail.blade.php
@@ -7,26 +7,26 @@
 @endsection
 
 <style>
-    ul {
+    .prod-images-slider ul {
         list-style: none outside none;
         padding-left: 0;
         margin-bottom: 0;
     }
 
-    li {
+    .prod-images-slider li {
         display: block;
         float: left;
         margin-right: 6px;
         cursor: pointer;
     }
 
-    img {
+    .prod-images-slider img {
         display: block;
         height: auto;
         max-width: 100%;
     }
 
-    .demo {
+    .prod-images-slider {
         border: 1px solid #B7B7B7;
         border-radius: 20px;
         padding: 12px 36px;
@@ -145,6 +145,80 @@
         height: 35px;
         object-fit: contain;
     }
+
+    .prod-detail-section .add-wishlist-btn {
+        visibility: visible;
+        padding: 8px 12px;
+        position: unset;
+    }
+    input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+    /* display: none; <- Crashes Chrome on hover */
+    -webkit-appearance: none;
+    margin: 0; /* <-- Apparently some margin are still there even though it's hidden */
+}
+
+.prod-detail-section #number {
+    text-align: center;
+    background: #F2F2F2;
+    font-weight: 700;
+    border: none;
+    border-left: 1px solid #D9D9D9;
+    border-right: 1px solid #D9D9D9;
+    width: 65px;
+}
+
+.prod-detail-section .prod-counter {
+    background: #F2F2F2;
+    border: none;
+    padding: 6px 10px;
+}
+.prod-detail-section .prod-counter span {
+    color: #FFF;
+    background: #000000;
+    padding: 5px;
+    border-radius: 50px;
+    width: 25px;
+    height: 25px;
+    font-size: 18px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.prod-detail-section .breadcrumb-item {
+    font-size: 15px;
+}
+.prod-detail-section .breadcrumb-item.active {
+    color: #000;
+    font-weight: 700;
+}
+.prod-detail-section .breadcrumb-item a:hover {
+    color: #b0191e;
+    text-decoration: underline;
+}
+
+.prod-images-slider .dropdown-toggle::after {
+    display: none;
+}
+
+.prod-images-slider .dropdown-toggle {
+    white-space: nowrap;
+    border: 1px solid #000;
+    background: none;
+    color: #000;
+    border-radius: 0;
+    text-transform: uppercase;
+    font-size: 15px;
+    padding: 8px 16px;
+    font-weight: 500;
+}
+.prod-images-slider .dropdown-toggle:hover, .prod-images-slider .dropdown-toggle:focus, .prod-images-slider .dropdown-toggle:active {
+    background: none;
+    color: #000;
+}
+.prod-images-slider .dropdown-toggle:focus, .prod-images-slider .dropdown-toggle:active {
+    box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.25);
+}
 </style>
 
 @section('public-content')
@@ -155,16 +229,16 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-4 px-2">
-                    <div class="demo">
-                        <div class="dropdown">
+                    <div class="prod-images-slider">
+                        <div class="dropdown text-end">
                             <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
                                 data-bs-toggle="dropdown" aria-expanded="false">
-                                Share
+                                <span class="fa fa-share me-1"></span> Share
                             </button>
                             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <li class="w-100"><a class="dropdown-item" href="#">Action</a></li>
+                                <li class="w-100"><a class="dropdown-item" href="#">Another action</a></li>
+                                <li class="w-100"><a class="dropdown-item" href="#">Something else here</a></li>
                             </ul>
                         </div>
                         <ul id="lightSlider">
@@ -189,10 +263,19 @@
                 <div class="col-lg-8 px-2">
                     <div class="prod-detail-section">
                         <div class="px-3 pt-3 pb-4">
-                            Home / Electric Motors / Servo Motors & Accessories / Servo Accessories / Electric Motor 1 Phase
-                            <h5>Mophorn 3 HP Electric Motor 1 Phase AC Motor 3450rpm 60Hz 56 Frame SPL Rot-CCW Air Compressor Motor,
-                                115/230V</h5>
-                            <div class="my-4 d-flex align-items-center">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="#">Electric Motors</a></li>
+                                    <li class="breadcrumb-item"><a href="#">Servo Motors & Accessories</a></li>
+                                    <li class="breadcrumb-item"><a href="#">Servo Accessories</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Electric Motor 1 Phase</li>
+                                </ol>
+                            </nav>
+
+                            <h3 class="col-lg-11">Mophorn 3 HP Electric Motor 1 Phase AC Motor 3450rpm 60Hz 56 Frame SPL Rot-CCW Air Compressor Motor,
+                                115/230V</h3>
+                            <div class="mt-4 d-flex align-items-center">
                                 <img src="{{ asset('public/uploads/website-images/images/rating-star1.png') }}" class="me-1">
                                 <img src="{{ asset('public/uploads/website-images/images/rating-star1.png') }}" class="me-1">
                                 <img src="{{ asset('public/uploads/website-images/images/rating-star1.png') }}" class="me-1">
@@ -201,10 +284,21 @@
                                 <b class="ms-1">4.0 <a href="#" class="mx-2 text-decoration-underline red-link">429 Reviews</a> | See More by <a href="#" class="text-decoration-underline red-link">Supplier</a></b>
                             </div>
 
-                            <div class="d-flex align-items-center">
+                            <div class="my-4 d-flex align-items-center">
                                 <h3 class="d-inline-block red-text">$198.00</h3>
-                                <s>$230.00</s>
-                                <span class="red-text">14%</span>OFF
+                                <s class="ms-3 me-2">$230.00</s>
+                                <b class="red-text">14% OFF</b>
+                            </div>
+
+                            <div class="d-flex">
+                                <div class="d-flex">
+                                    <button class="decrement prod-counter"><span class="fa fa-minus icon"></span></button>
+                                    <input type="number" id="number" value="0">
+                                    <button class="increment prod-counter"><span class="fa fa-plus icon"></span></button>
+                                </div>
+
+                                <button class="ms-3 me-2 add-wishlist-btn"><span class="fa fa-heart-o wishlist-icon" aria-hidden="true"></span></button>
+                                <button class="btn-bg add-cart-btn">Add to Cart</button>
                             </div>
 
                             <p class="my-3">Select Size: <b>3 HP</b></p>
@@ -368,7 +462,7 @@
                             <p class="py-2 text-center small list"><b>Frequency: </b>60hz</p>
                         </div>
                         <div class="py-3 px-2">
-                            <button class="btn btn-bg add-cart-btn w-100 rounded-0">Add to Cart</button>
+                            <button class="btn-bg add-cart-btn w-100">Add to Cart</button>
                             <button class="mt-2 black-btn w-100">View Details</button>
                         </div>
                     </div>
@@ -391,7 +485,7 @@
                             <p class="py-2 text-center small list"><b>Frequency: </b>60hz</p>
                         </div>
                         <div class="py-3 px-2">
-                            <button class="btn btn-bg add-cart-btn w-100 rounded-0">Add to Cart</button>
+                            <button class="btn-bg add-cart-btn w-100">Add to Cart</button>
                             <button class="mt-2 black-btn w-100">View Details</button>
                         </div>
                     </div>
@@ -414,7 +508,7 @@
                             <p class="py-2 text-center small list"><b>Frequency: </b>60hz</p>
                         </div>
                         <div class="py-3 px-2">
-                            <button class="btn btn-bg add-cart-btn w-100 rounded-0">Add to Cart</button>
+                            <button class="btn-bg add-cart-btn w-100">Add to Cart</button>
                             <button class="mt-2 black-btn w-100">View Details</button>
                         </div>
                     </div>
@@ -437,7 +531,7 @@
                             <p class="py-2 text-center small list"><b>Frequency: </b>60hz</p>
                         </div>
                         <div class="py-3 px-2">
-                            <button class="btn btn-bg add-cart-btn w-100 rounded-0">Add to Cart</button>
+                            <button class="btn-bg add-cart-btn w-100">Add to Cart</button>
                             <button class="mt-2 black-btn w-100">View Details</button>
                         </div>
                     </div>
@@ -460,7 +554,7 @@
                             <p class="py-2 text-center small list"><b>Frequency: </b>60hz</p>
                         </div>
                         <div class="py-3 px-2">
-                            <button class="btn btn-bg add-cart-btn w-100 rounded-0">Add to Cart</button>
+                            <button class="btn-bg add-cart-btn w-100">Add to Cart</button>
                             <button class="mt-2 black-btn w-100">View Details</button>
                         </div>
                     </div>
@@ -483,7 +577,7 @@
                             <img src="{{ asset('public/uploads/website-images/images/engine15.png') }}">
                             <a href="" class="position-absolute text-white quick-view">Quick View</a>
                         </div>
-                        <button class="btn btn-bg add-cart-btn w-100 rounded-0">Add to Cart</button>
+                        <button class="btn-bg add-cart-btn w-100">Add to Cart</button>
                         <div class="p-3">
                             <h6 class="mb-2">Release device EM 24V-DC</h6>
                             <span class="price">$377.00</span>
@@ -499,7 +593,7 @@
                             <img src="{{ asset('public/uploads/website-images/images/engine2.png') }}">
                             <a href="" class="position-absolute text-white quick-view">Quick View</a>
                         </div>
-                        <button class="btn btn-bg add-cart-btn w-100 rounded-0">Add to Cart</button>
+                        <button class="btn-bg add-cart-btn w-100">Add to Cart</button>
                         <div class="p-3">
                             <h6 class="mb-2">Release device EM 24V-DC</h6>
                             <span class="price">$377.00</span>
@@ -516,7 +610,7 @@
                             <img src="{{ asset('public/uploads/website-images/images/engine3.png') }}">
                             <a href="" class="position-absolute text-white quick-view">Quick View</a>
                         </div>
-                        <button class="btn btn-bg add-cart-btn w-100 rounded-0">Add to Cart</button>
+                        <button class="btn-bg add-cart-btn w-100">Add to Cart</button>
                         <div class="p-3">
                             <h6 class="mb-2">Release device EM 24V-DC</h6>
                             <span class="price">$377.00</span>
@@ -529,10 +623,10 @@
                                 aria-hidden="true"></span></button>
                         <div
                             class="position-relative text-center d-flex justify-content-center align-items-center img-holder">
-                            <img src="{{ asset('public/uploads/website-images/images/engine8.png') }}">
+                            <img src="{{ asset('public/uploads/website-images/images/engine9.png') }}">
                             <a href="" class="position-absolute text-white quick-view">Quick View</a>
                         </div>
-                        <button class="btn btn-bg add-cart-btn w-100 rounded-0">Add to Cart</button>
+                        <button class="btn-bg add-cart-btn w-100">Add to Cart</button>
                         <div class="p-3">
                             <h6 class="mb-2">Release device EM 24V-DC</h6>
                             <span class="price">$377.00</span>
@@ -554,10 +648,10 @@
                                 aria-hidden="true"></span></button>
                         <div
                             class="position-relative text-center d-flex justify-content-center align-items-center img-holder">
-                            <img src="{{ asset('public/uploads/website-images/images/engine15.png') }}">
+                            <img src="{{ asset('public/uploads/website-images/images/engine16.png') }}">
                             <a href="" class="position-absolute text-white quick-view">Quick View</a>
                         </div>
-                        <button class="btn btn-bg add-cart-btn w-100 rounded-0">Add to Cart</button>
+                        <button class="btn-bg add-cart-btn w-100">Add to Cart</button>
                         <div class="p-3">
                             <h6 class="mb-2">Release device EM 24V-DC</h6>
                             <span class="price">$377.00</span>
@@ -570,10 +664,10 @@
                                 aria-hidden="true"></span></button>
                         <div
                             class="position-relative text-center d-flex justify-content-center align-items-center img-holder">
-                            <img src="{{ asset('public/uploads/website-images/images/engine2.png') }}">
+                            <img src="{{ asset('public/uploads/website-images/images/engine5.png') }}">
                             <a href="" class="position-absolute text-white quick-view">Quick View</a>
                         </div>
-                        <button class="btn btn-bg add-cart-btn w-100 rounded-0">Add to Cart</button>
+                        <button class="btn-bg add-cart-btn w-100">Add to Cart</button>
                         <div class="p-3">
                             <h6 class="mb-2">Release device EM 24V-DC</h6>
                             <span class="price">$377.00</span>
@@ -587,10 +681,10 @@
                                 aria-hidden="true"></span></button>
                         <div
                             class="position-relative text-center d-flex justify-content-center align-items-center img-holder">
-                            <img src="{{ asset('public/uploads/website-images/images/engine3.png') }}">
+                            <img src="{{ asset('public/uploads/website-images/images/engine17.png') }}">
                             <a href="" class="position-absolute text-white quick-view">Quick View</a>
                         </div>
-                        <button class="btn btn-bg add-cart-btn w-100 rounded-0">Add to Cart</button>
+                        <button class="btn-bg add-cart-btn w-100">Add to Cart</button>
                         <div class="p-3">
                             <h6 class="mb-2">Release device EM 24V-DC</h6>
                             <span class="price">$377.00</span>
@@ -606,7 +700,7 @@
                             <img src="{{ asset('public/uploads/website-images/images/engine8.png') }}">
                             <a href="" class="position-absolute text-white quick-view">Quick View</a>
                         </div>
-                        <button class="btn btn-bg add-cart-btn w-100 rounded-0">Add to Cart</button>
+                        <button class="btn-bg add-cart-btn w-100">Add to Cart</button>
                         <div class="p-3">
                             <h6 class="mb-2">Release device EM 24V-DC</h6>
                             <span class="price">$377.00</span>
@@ -621,6 +715,25 @@
         ==============================-->
     <script>
         $(function() {
+            $(document).on('click', '.increment', function() {
+                var number = $('#number').val();
+                number++;
+                $('#number').val(number);
+            });
+
+            $('.decrement').on('click', function() {
+                var number = $('#number').val();
+                if (number > 0) {
+                    number--;
+                    $('#number').val(number);
+                }
+            });
+
+            $(document).on('click', '.prod-size', function(){
+                $('.prod-size').removeClass('active');
+                $(this).addClass('active');
+            });
+
             $('#lightSlider').lightSlider({
                 gallery: true,
                 item: 1,
@@ -628,8 +741,8 @@
                 slideMargin: 0,
                 thumbItem: 9,
                 controls: false
-                // prevHtml: "<b>www</b>",
-                // nextHtml: "<b>eee</b>",
+                // prevHtml: "<b></b>",
+                // nextHtml: "<b></b>",
             });
         });
     </script>
